180e5627a9bca0f523071332603b1f910294e481
dundole <dundole@naver.com>
2022-01-25 20:28:11 +0900

    content

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
         "title": "test",
-        "excerpt":"Neural News Recommendation with Multi-Head Self-Attention EMNLP-IJCNLP 2019 | Nov 2019 Wu. Chuhan, Wu. Fangzhao, Ge. Suyu, Qi. Tao, Huang. Yongfeng, Xie. Xing Tsinghua University, Microsoft Research Asia DOI : 10.18653/v1/D19-1671 Abstract. 이 논문에서는 NRMS(multi-head self-attention)를 사용한 신경망 뉴스 추천 접근 방식을 제안한다. 저자들이 뉴스 추천에서 중요한 관측으로 여기는 몇...","categories": ["보안"],
+        "excerpt":"test  ","categories": ["보안"],
         "tags": ["test"],
         "url": "/%EB%B3%B4%EC%95%88/test/",
-        "teaser": null
-      },{
-        "title": "[ML/사이킷런] Cross Validation (교차검증)",
-        "excerpt":"Cross Validation이란? 교차검증, 머신러닝 모델을 평가할 땐 단순히 train set으로 모델을 학습하고, test set으로 검증을 하지만, 이런 방식을 이용하게 되면 모델이 test set에 과적합하는 문제가 발생할 수 있다. test set과 다른 데이터에 대해서는 제대로 된 결과를 도출해내지 못 할수 있다는 의미. 이를 해결하고자 하는 것이 Cross Validation, 즉 교차 검증이다....","categories": ["머신러닝"],
-        "tags": ["ML"],
-        "url": "/%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D/Cross-Validation/",
         "teaser": null
       }]
